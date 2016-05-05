c62b1bc58c74fd1bef6fda9dd503d1d0bfb884da
Howard Paget <hejp89@users.noreply.github.com>
2016-05-05 19:45:44 +0100

    Example update
    Added circuit diagram to example.

--- example/example.py
+++ example/example.py
@@ -4,40 +4,55 @@ import pyparticle as pp
 import sys
 import os
 
-currenr_dir = os.path.dirname(__file__)
+# Read the access token stored in a file
+current_dir = os.path.dirname(__file__)
 token_filename = os.path.join(currenr_dir, 'access_token.txt')
 
 access_token_file = open(token_filename, 'r')
 access_token = access_token_file.read().strip()
 access_token_file.close()
 
+# Initiatise the Particle object using the cache access token
 particle = pp.Particle(access_token=access_token)
 
 try:
     devices = particle.list_devices()
 except:
+    # An exception has been raised indicating that the access token is out of date use the login details to refresh the token
     particle = pp.Particle('username', 'password')
 
+    # Cache the token for reuse
     access_token_file = open(token_filename, 'w')
     access_token = access_token_file.write(particle.access_token)
     access_token_file.close()
-    
+
     devices = particle.list_devices()
 
 if len(devices) == 0:
     print('No devices found')
     sys.exit()
-  
+
+# Use the first device in the list this device is running the dht-example.ino example
 device = devices[0]
+
+print('Found %d devices' % len(devices))
 
 print('Selected device: %s' % device['name'])
 
+# The device exposes the variables 'humidity', 'temp_dht', 'temp_bmp' and 'pressure'
 humidity = particle.get_variable(device['id'], 'humidity')
 temp_dht = particle.get_variable(device['id'], 'temp_dht')
 temp_bmp = particle.get_variable(device['id'], 'temp_bmp')
 pressure = particle.get_variable(device['id'], 'pressure')
 
+# Print the variables
 print('Humidity: %.2f%%' % humidity['result'])
 print('Temperature (DHT Sensor): %.2f°C' % temp_dht['result'])
 print('Temperature (BMP Sensor): %.2f°C' % temp_bmp['result'])
 print('Pressure: %.2fmb' % pressure['result'])
+
+# The device exposes the function set_led which takes in a command of either 'on' or 'off' and sets the LED to on or off
+led_value = particle.call_function(device['id'], 'set_led', 'on')
+
+# The set_led function returns a 0 or 1 if the LED is turned off or on, or -1 on an error e.g. invalid command
+print('LED Value: %d' % led_value)
--- pyparticle.py
+++ pyparticle.py
@@ -11,49 +11,49 @@ DEVICES_URL = PARTICLE_API_URL + '/v1/devices'
 
 class Particle():
 
-    # Initialise connection with Cloud API
-    # 
-    # If login details are provided a new access token will be generated otherwise the token given will be used.
+    # Initialise the  object used to interact with Particle Cloud API
+    #
+    # If login details are provided a new access token will be generated other the token given will be used.
     # The provided token is not checked for validity
     def __init__(self, username=None, password=None, access_token=None):
-        
+
         if username is not None and password is not None:
             self.username = username
             self.password = password
-        
+
             headers = {'Authorization': 'Basic %s' % base64.encodestring('particle:particle').replace('\n', '')}
-            
+
             data = {
                 'grant_type': 'password',
                 'username': self.username,
                 'password': self.password
             }
-                
-            try:      
+
+            try:
                 response_obj = self.api('POST', AUTH_TOKEN_URL, data=data, headers=headers)
             except:
                 raise
-    
+
             self.access_token = response_obj['access_token']
             self.access_token_expiry_date = dt.datetime.now() + dt.timedelta(seconds=response_obj['expires_in'])
-            self.refresh_token = response_obj['refresh_token'] 
- 
+            self.refresh_token = response_obj['refresh_token']
+
         elif access_token is not None:
             self.access_token = access_token
         else:
             raise ValueError("Username and password or access token must be supplied.")
-            
-    
+
+
     # Generic function to handle Particle Cloud API calls
     def api(self, method, url, data={}, params={}, headers={}):
-        
+
         if method.upper() == 'GET':
             params_str = '&'.join(['%s=%s' % (k, v) for k, v in params.items()])
-        
+
             response = requests.get(url + '?' + params_str)
         elif method.upper() == 'POST':
             response = requests.post(url, data=data, headers=headers)
-            
+
         response_obj = json.loads(response.text)
 
         if response.status_code != 200:
@@ -62,47 +62,47 @@ class Particle():
 
             if 'error' in response_obj:
                 raise Exception(response_obj['error'])
-        
+
         return response_obj
-        
-    # List devices the currently authenticated user has access to.
+
+    # List devices that the currently authenticated user has access to.
     #
     # Returns a dict containing the response: https://docs.particle.io/reference/api/#list-devices
     def list_devices(self):
 
-        try:        
+        try:
             devices_obj = self.api('GET', DEVICES_URL, params={'access_token': self.access_token})
         except:
             raise
 
         return devices_obj
 
-        
-    # Get the current value of a variable exposed by the device.
+
+    # Get the current value of a variable exposed by a device.
     #
     # Returns a dict containing the response: https://docs.particle.io/reference/api/#get-a-variable-value
-    def get_variable(self, device_id, variable_name):      
+    def get_variable(self, device_id, variable_name):
         url = ''.join([DEVICES_URL, '/', device_id, '/', variable_name])
-                
-        try:        
+
+        try:
             variable_obj = self.api('GET', url, params={'access_token': self.access_token})
         except:
             raise
 
         return variable_obj
-    
-    # Call a function exposed by the device.
+
+    # Call a function exposed by a device.
     #
     # Returns a dict containing the response: https://docs.particle.io/reference/api/#call-a-function
-    def call_function(self, device_id, function_name, arg, raw=False):      
+    def call_function(self, device_id, function_name, arg, raw=False):
         url = ''.join([DEVICES_URL, '/', device_id, '/', function_name])
-                
+
         try:
             if raw:
-                return_value_obj = self.api('POST', url, data={'arg': arg,' format': 'raw', 'access_token': self.access_token})  
+                return_value_obj = self.api('POST', url, data={'arg': arg,' format': 'raw', 'access_token': self.access_token})
             else:
-                return_value_obj = self.api('POST', url, data={'arg': arg,'access_token': self.access_token})  
+                return_value_obj = self.api('POST', url, data={'arg': arg, 'access_token': self.access_token})
         except:
             raise
 
-        return return_value_obj
+        return return_value_obj['return_value']
