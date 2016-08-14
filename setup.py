861378f38aefc702bef68ecf05fdf6b5608fb47f
Howard Paget <hejp89@users.noreply.github.com>
2016-08-14 19:53:27 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyparticle',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='hejp89',
+    author_email='',
+    description=''
+)
